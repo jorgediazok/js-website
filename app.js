0187b22de541ca9f58cff9b0a4849021f55cda0c
Jorge <jorgediazok@gmail.com>
2020-05-05 04:37:20 -0300

    animations added

--- app.js
+++ app.js
@@ -17,7 +17,21 @@ function animateSlides() {
       defaults: { duration: 1, ease: 'power2.inOut' },
     });
     slideTl.fromTo(revealImg, { x: '0%' }, { x: '100%' });
-    slideTl.fromTo(img, { scale: 2 }, { scale: 1 });
+    slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, '-=1');
+    slideTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, '-=0.5');
+    slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=0.5');
+    //Create Scene
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+    })
+      .setTween(slideTl)
+      .addIndicators({
+        colorStart: 'white',
+        colorTrigger: 'white',
+        name: 'slide',
+      })
+      .addTo(controller);
   });
 }
 
